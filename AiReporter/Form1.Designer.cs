9f91a1becc5d5a36811525f01b2368dc474457ff
Arman.Mohebbi <Arman.Mohebbi@PSM-LP03>
2025-04-13 14:13:10 +0330

    Add project files.

---
+++ AiReporter/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AiReporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt1 = new TextBox();
+            btn1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            panelHtmlPreview = new Panel();
+            button3 = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // txt1
+            // 
+            txt1.Location = new Point(12, 37);
+            txt1.Multiline = true;
+            txt1.Name = "txt1";
+            txt1.Size = new Size(330, 431);
+            txt1.TabIndex = 0;
+            txt1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(348, 37);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(102, 23);
+            btn1.TabIndex = 1;
+            btn1.Text = "<=پیشنهاد";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(884, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 2;
+            label1.Text = "راه حل پیشنهادی";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(268, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 2;
+            label2.Text = "صورت مسئله";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(710, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(348, 445);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 23);
+            button1.TabIndex = 1;
+            button1.Text = "درباره";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btn1_Click;
+            // 
+            // panelHtmlPreview
+            // 
+            panelHtmlPreview.Location = new Point(456, 37);
+            panelHtmlPreview.Name = "panelHtmlPreview";
+            panelHtmlPreview.Size = new Size(515, 431);
+            panelHtmlPreview.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(348, 416);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 23);
+            button3.TabIndex = 1;
+            button3.Text = "راهنما";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += btn1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(389, 500);
+            label4.Name = "label4";
+            label4.Size = new Size(582, 15);
+            label4.TabIndex = 2;
+            label4.Text = "توجه: \"این سیستم هوش مصنوعی به صورت آزمایشی پیاده‌سازی شده و نتایج آن ممکن است نیاز به بازنگری داشته باشد.\"";
+            label4.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(983, 524);
+            Controls.Add(panelHtmlPreview);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(btn1);
+            Controls.Add(txt1);
+            Name = "Form1";
+            RightToLeft = RightToLeft.Yes;
+            Text = "سیستم هوش مصنوعی پتروسازه مبین  (طرح آزمایشی)";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt1;
+        private Button btn1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Panel panelHtmlPreview;
+        private Button button3;
+        private Label label4;
+    }
+}
---
+++ AiReporter/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+using Newtonsoft.Json;
+
+namespace AiReporter
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "AIzaSyDdG3eewfkI9Drz2BJILfTHu9zdbNgOUSA"; // کلید API
+        private WebView2 _webView;
+        string  generatedText;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromMinutes(5)
+            };
+
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            _webView = new WebView2
+            {
+                Dock = DockStyle.Fill
+            };
+
+            await _webView.EnsureCoreWebView2Async();
+            panelHtmlPreview.Controls.Add(_webView); // اضافه به پنل نه فرم
+        }
+
+        private async void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var inputText = txt1.Text.Trim();
+                if (string.IsNullOrWhiteSpace(inputText))
+                {
+                    MessageBox.Show("لطفاً متن اولیه Claim را وارد کنید.");
+                    return;
+                }
+
+                label3.Invoke(() => label3.Text = "در حال پردازش... لطفاً صبر کنید...");
+
+                var requestBody = new
+                {
+                    contents = new[]
+                    {
+                        new
+                        {
+                            parts = new[]
+                            {
+                                new { text = "شما یک متخصص قراردادهای EPC هستید.\n\nلطفاً متن زیر را به زبان فارسی، به صورت رسمی، ویرایش و بازنویسی کنید. سپس آن را با HTML تمیز، دارای استایل و رنگ، برای نمایش وب آماده نمایید.\n\nفقط خروجی HTML نهایی را بدون هیچ توضیحی بازگردانید." },
+                                new { text = inputText },
+                               new{ text = "نام شرکت پترو سازه مبین است در متن پیشنهادی اگر نیاز بود لحاظ کنن"}
+                            }
+                        }
+                    }
+                };
+
+                var jsonRequestBody = JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
+
+                var url = $"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={_apiKey}";
+                var response = await _httpClient.PostAsync(url, content).ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    //  txt2.Invoke(() => txt2.Text = errorContent);
+                    MessageBox.Show($"خطا در ارتباط با هوش مصنوعی: {errorContent}");
+                    return;
+                }
+
+                var result = await response.Content.ReadAsStringAsync();
+                dynamic jsonResponse = JsonConvert.DeserializeObject(result); 
+
+                generatedText = jsonResponse?.candidates?[0]?.content?.parts?[0]?.text?.ToString();
+                generatedText = generatedText.Replace("```", "");
+                generatedText = generatedText.Replace("HTML", "");
+                generatedText = generatedText.Replace("html", "");
+                generatedText = generatedText.Replace("< lang=\"fa\">\r\n", "");
+                generatedText = generatedText.Replace("< lang=\"fa\">", "");
+                generatedText = generatedText.Replace("< lang=\"fa\" dir=\"rtl\">\r\n", "");
+
+                if (string.IsNullOrWhiteSpace(generatedText))
+                {
+                    label3.Invoke(() => label3.Text = "پاسخی دریافت نشد.");
+                    return;
+                }
+
+                label3.Invoke(() => label3.Text = "پردازش تکمیل شد");
+                label4.Invoke(() => label4.Visible = true);
+
+                // نمایش HTML در WebView2
+                _webView.Invoke(() => _webView.NavigateToString(generatedText));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در پردازش: {ex.Message}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+
+    public static class ControlExtensions
+    {
+        public static void Invoke(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+                control.Invoke(action);
+            else
+                action();
+        }
+    }
+}
